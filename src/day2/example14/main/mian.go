a3cdbd843d89a18d899f6db4d285a3e5c47ca20b
abner <abner.dang@qingclass.com>
2018-03-09 17:58:47 +0800

    generate random num

--- src/day2/example14/main/mian.go
+++ src/day2/example14/main/mian.go
@@ -3,32 +3,42 @@ package main
 import(
 	"math/rand"
 	"fmt"
-	"time"
 )
 
 func main()  {
 	var intChannel = make(chan int, 10)
 	var floatChannel = make(chan float32, 10)
+	fmt.Println("=======整数==========")
 	for i := 0;i< 10; i++  {
 		go genRandomInt(intChannel)
 		x := <- intChannel
 		fmt.Println(x)
 	}
-	fmt.Println("==============================")
+	fmt.Println("======小于10的整数=======")
+	for i := 0;i< 10; i++  {
+		go genRandomIntLess100(intChannel)
+		x := <- intChannel
+		fmt.Println(x)
+	}
+	fmt.Println("========浮点数===========")
 	for i := 0; i < 10; i ++ {
 		go genRandomFloat(floatChannel)
 		f := <- floatChannel
 		fmt.Println(f)
 	}
 
-	time.Sleep(time.Second * 1)
 
 }
-
+// 生成随机数并放入管道
 func genRandomInt(c chan int ) {
 	c <- rand.Int()
 }
-
+// 生成浮点数并放入管道
 func genRandomFloat(c chan float32) {
 	c <- rand.Float32()
+}
+
+// 生成小于10的随机整数
+func genRandomIntLess100(c chan int){
+		c <- rand.Intn(10)
 }
